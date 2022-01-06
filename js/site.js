cb327f2c8657d55938287fb83cecdc0bf0be9cfc
Alex Aveldanez <alexsaveldanez@gmail.com>
2022-01-06 14:12:06 -0800

    Updated UI
    Updated UI with app name and logo.

--- js/site.js
+++ js/site.js
@@ -1,4 +1,44 @@
+// Get values from the user. We need to get the fizz and the buzz value.
+function getValues() {
 
-function HelloWorld() {
-    alert("Hello World");
+    // get the user values from the DOM
+
+    // check for numbers
+
+    // check that the numbers are integers
+
+    // we can call fizzBuzz
+
+    // call displayData and write the values to the screen
+}
+
+function fizzBuzz(fizzValue, buzzValue) {
+
+    let returnArray = [];
+
+    // initialize the return array
+
+    // loop from 1 to 100
+
+
+    // we need to check the current value in three steps
+    // check to see if divisible by Both (3 and 5)
+    // if so push 'FizzBuzz' into an array and not the number
+
+    // check to see if divisible by fizz value (3)
+    // if so push 'Fizz' into an array and not the number
+
+    // check to see if divisible by buzz value (5)
+    // if so push 'Buzz' into an array and not the number
+
+    // if none then push the number into the array
+
+    return returnArray;
+}
+
+function displayData(fbArray) {
+    
+    // loop over the array and create a tablerow for each item.
+
+    // add all the rows to the table.
 }
